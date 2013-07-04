564b5b925670eb8eec3f24fee12ba6c339e8c54f
Blake Rain <blake.rain@massivetactical.com>
2013-07-04 11:20:00 +0100

    Modifications to application main entry point.
    Signed-off-by: Blake Rain <blake.rain@massivetactical.com>

--- app/main.hs
+++ app/main.hs
@@ -1,8 +1,11 @@
-import Prelude              (IO)
+import Prelude              (IO, ($))
+import System.Environment   (withArgs)
 import Yesod.Default.Config (fromArgs)
 import Yesod.Default.Main   (defaultMain)
 import Settings             (parseExtra)
 import Application          (makeApplication)
 
 main :: IO ()
-main = defaultMain (fromArgs parseExtra) makeApplication
+main =
+  withArgs ["Development"] $
+    defaultMain (fromArgs parseExtra) makeApplication
